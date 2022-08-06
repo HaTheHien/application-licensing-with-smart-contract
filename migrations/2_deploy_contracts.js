a8c65bc36f5ca981cddc7a2326ffe9b2717d8073
HaTheHien <47638822+HaTheHien@users.noreply.github.com>
2022-08-06 16:26:53 +0700

    fix application contract

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,7 +1,9 @@
 var SolidityDrive = artifacts.require("./SolidityDrive.sol");
 var License = artifacts.require("./License.sol");
+var Application = artifacts.require("./Application.sol");
 
 module.exports = function(deployer) {
   deployer.deploy(SolidityDrive);
   deployer.deploy(License);
+  deployer.deploy(Application);
 };
